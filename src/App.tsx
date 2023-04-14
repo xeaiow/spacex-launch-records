1f0ea3c89c6c784cd9b268d6cea27509549cbdf6
xeaiow <xeaiow@gmail.com>
2023-04-14 22:24:27 +0800

    Replace default styles with Bootstrap framework

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import LaunchList from './pages/LaunchList'
+import LaunchList from './pages/launchList';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 function App() {
   return (
     <LaunchList />
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import client from './common/apollo-client'
-import { ApolloProvider } from '@apollo/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import client from './common/apollo-client';
+import { ApolloProvider } from '@apollo/client';
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
@@ -11,4 +10,4 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
       <App />
     </ApolloProvider>
   </React.StrictMode>,
-)
+);
